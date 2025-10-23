38e014d07041059aae85e76c3f710d11ea361a26
Richard Sperry <richard@sperryfamily.net>
2025-10-23 02:33:18 -0700

    update

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -5,6 +5,6 @@
 # ps /etc/cups
 # if [ ! -d /config/cups ]; then cp -v -R /etc/cups /config; fi
 # ln -v -s /config/cu
-bashio::log.info "Starting CUPS server as CMD from S6"
+bashio::log.info "Starting CUPS server as CMD from docker"
 
 cupsd -f
