be0c1c1137e8ae9d3ad212934185d8dc4034ef16
sysop <sysop@kali.localdomain>
2025-10-24 17:40:12 -0400

    update

--- airprint/printer-update.sh
+++ airprint/printer-update.sh
@@ -3,10 +3,9 @@
 [ "yes" = "${AVAHI_FRIENDLY_DESC}" ] && FLAGS="-x" || FLAGS=""
 
 inotifywait -m -e close_write,moved_to,create /etc/cups |
-
-while read -r directory events filename; do
-	if [ "$filename" = "printers.conf" ]; then
-    rm -rf /etc/avahi/services/AirPrint-*.service
-		/opt/airprint/airprint-generate.py -d /etc/avahi/services ${FLAGS}
-	fi
-done
+    while read -r directory events filename; do
+        if [ "$filename" = "printers.conf" ]; then
+            rm -rf /etc/avahi/services/AirPrint-*.service
+            /opt/airprint/airprint-generate.py -d /etc/avahi/services "${FLAGS}"
+        fi
+    done
